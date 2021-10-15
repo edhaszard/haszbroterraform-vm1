7d940e6c802dc050c25f0fff58f07f891d56aaf3
Ed Haszard Morris <ed.haszard@ihc.org.nz>
2021-10-15 17:37:01 +1300

    vm additions, errors for sure

---
+++ az_windows_vm/main.tf
@@ -0,0 +1,135 @@
+##########################################################
+## Create NIC's for VM
+##########################################################
+resource "azurerm_network_interface" "net_if" {
+  count               = var.vm_count
+  location            = var.location
+  name                = "${var.vm_name}${count.index + 1}-nic"
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "ipconfmain"
+    subnet_id                     = var.vm_subnet
+    private_ip_address_allocation = "Static"
+    private_ip_address = "${var.ip_address_range}${count.index + 6}"
+  }
+  tags = {
+    project     = var.tag_project
+    environment = var.tag_environment
+    comments    = var.tag_comments
+    deployment  = var.tag_deployment
+  }
+}
+
+
+##########################################################
+## Create Windows VM
+##########################################################
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                             = "${var.vm_name}${count.index + 1}"
+  count                            = var.vm_count
+  location                         = var.location
+  resource_group_name              = var.resource_group_name
+  size                             = var.vm_size
+  admin_username                   = var.vm_admin_username
+  admin_password                   = var.vm_admin_password
+  network_interface_ids            = [element(azurerm_network_interface.net_if.*.id, count.index)]
+  availability_set_id              = var.avset_id
+  # delete_data_disks_on_termination = true
+
+  source_image_reference {
+    offer     = var.vm_storage_img_offer
+    publisher = var.vm_storage_img_publisher
+    sku       = var.vm_storage_img_sku
+    version   = "latest"
+  ### Reference to custom image
+    # id = data.azurerm_image.vmimage.id
+  }
+
+  os_disk {
+    name              = "${var.vm_name}${count.index + 1}-os"
+    storage_account_type = var.storage_account_type
+    caching           = "ReadWrite"
+  }
+  
+  tags = {
+    project     = var.tag_project
+    environment = var.tag_environment
+    comments    = var.tag_comments
+    deployment  = var.tag_deployment
+  }
+
+}
+
+# ##########################################################
+# ### Azure Monitoring Agent
+# ##########################################################
+# resource "azurerm_virtual_machine_extension" "vmagent" {
+#   count                = var.vm_count
+#   name                 = "MicrosoftMonitoringAgent"
+#   virtual_machine_id   = element(azurerm_windows_virtual_machine.vm.*.id, count.index)
+#   publisher            = "Microsoft.EnterpriseCloud.Monitoring"
+#   type                 = "MicrosoftMonitoringAgent"
+#   type_handler_version = "1.0"
+#   depends_on           = [azurerm_windows_virtual_machine.vm]
+#   settings = <<SETTINGS
+#   {
+#     "workspaceId": "bc7dd49a-2206-461c-9164-619c6471eca2"
+#   }
+#   SETTINGS
+
+#   protected_settings = <<PROTECTED_SETTINGS
+#     {
+#         "workspaceKey": "${var.workspace_key}"
+#     }
+# PROTECTED_SETTINGS
+# }
+
+
+# ##########################################################
+# ## Join VM to domain
+# ##########################################################
+# resource "azurerm_virtual_machine_extension" "adjoin" {
+#   count                = var.vm_count
+#   name                 = "join-domain"
+#   virtual_machine_id   = element(azurerm_windows_virtual_machine.vm.*.id, count.index)
+#   publisher            = "Microsoft.Compute"
+#   type                 = "JsonADDomainExtension"
+#   type_handler_version = "1.3"
+#   depends_on           = [azurerm_windows_virtual_machine.vm]
+
+#   settings = <<SETTINGS
+#     {
+#         "Name": "ihc.co.nz",
+#         "OUPath": "OU=Azure,OU=Hardware,DC=ihc,DC=co,DC=nz",
+#         "User": "ihc\\svc_ADjoin",
+#         "Restart": "true",
+#         "Options": "3"
+#     }
+# SETTINGS
+
+#   protected_settings = <<PROTECTED_SETTINGS
+#     {
+#         "Password": "${var.ad_join_password}"
+#     }
+# PROTECTED_SETTINGS
+# }
+
+# ##########################################################
+# ## Install Roles on VM
+# ##########################################################
+# resource "azurerm_virtual_machine_extension" "ad-tools" {
+#   count                = var.vm_count
+#   name                 = "install-adtools"
+#   virtual_machine_id   = element(azurerm_windows_virtual_machine.vm.*.id, count.index)
+#   publisher            = "Microsoft.Compute"
+#   type                 = "CustomScriptExtension"
+#   type_handler_version = "1.9"
+#   depends_on           = [azurerm_windows_virtual_machine.vm]
+
+#   settings = <<SETTINGS
+#     { 
+#       "commandToExecute": "powershell Add-WindowsFeature -Name NPAS;Add-WindowsFeature -Name RSAT-NPAS"
+#     } 
+# SETTINGS
+# }
--- main.tf
+++ main.tf
@@ -42,3 +42,34 @@ module "network1" {
   network_vnet_name = var.network_vnet_name
   network_subnet_name = var.network_subnet_name
 }
+
+###################################################
+## WINDOWS VM MODULE
+###################################################
+module "azvm1" {
+  source               = ".//modules/az_windows_vm"
+  location             = module.resourcegr.location
+  resource_group_name  = module.resourcegr.resource_group_name
+  vm_count             = 2
+  vm_name              = "prd-raid-0"
+  vm_size              = "Standard_D2s_v3"
+  storage_account_type = "StandardSSD_LRS"
+  data_disk_size       = "100"
+  data_disk_type       = "StandardSSD_LRS"
+  vm_subnet            = module.network.subnet1_id
+  avset_id             = module.azavset.avset_id
+  ip_address_range    = "10.200.0."
+  vm_admin_username    = var.vm_admin_username
+  vm_admin_password    = var.vm_admin_password
+  ad_join_password     = var.ad_join_password
+  workspace_key        = var.workspace_key
+  ### AZURE PUBLIC IMAGE
+  vm_storage_img_publisher = "MicrosoftWindowsServer"
+  vm_storage_img_offer     = "WindowsServer"
+  vm_storage_img_sku       = "2019-datacenter-gensecond"
+  ### TAGS
+  tag_project     = module.resourcegr.tag_project
+  tag_environment = module.resourcegr.tag_environment
+  tag_comments    = module.resourcegr.tag_comments
+  tag_deployment  = module.resourcegr.tag_deployment
+}
