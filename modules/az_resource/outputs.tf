7c72b253dedd3855d1f55972e6db0ee17c127313
Ed Haszard Morris <ed.haszard@ihc.org.nz>
2021-10-15 22:03:16 +1300

    added remaining missing tag variables to RG outputs.tf

--- modules/az_resource/outputs.tf
+++ modules/az_resource/outputs.tf
@@ -9,3 +9,15 @@ output "rg_name" {
 output "tag_deployment" {
   value = var.tag_deployment
 }
+
+output "tag_project" {
+  value = var.tag_project
+}
+
+output "tag_environment" {
+  value = var.tag_environment
+}
+
+output "tag_comments" {
+  value = var.tag_comments
+}
