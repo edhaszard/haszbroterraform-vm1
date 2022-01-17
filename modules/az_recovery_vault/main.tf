1ba6ef7b6032cf131d64366f532d671ae655019d
Ed Haszard Morris <ed.haszard@ihc.org.nz>
2022-01-17 13:58:46 +1300

    added SKU for vault

--- modules/az_recovery_vault/main.tf
+++ modules/az_recovery_vault/main.tf
@@ -10,4 +10,5 @@ resource "azurerm_recovery_services_vault" "vault" {
     name = "rv-test-${data.azurerm_resource_group.ASR_rg.location}"
     location = data.azurerm_resource_group.ASR_rg.location
     resource_group_name = data.azurerm_resource_group.ASR_rg.name
+    sku = ASR_sku
 }
