aaa407db02e9ddcd804fc5d57e3dde3367048027
Ed Haszard Morris <ed.haszard@ihc.org.nz>
2022-01-17 13:38:39 +1300

    added reg name argument to vault module

--- modules/az_recovery_vault/main.tf
+++ modules/az_recovery_vault/main.tf
@@ -9,4 +9,5 @@ data "azurerm_resource_group" "ASR_rg" {
 resource "azurerm_recovery_services_vault" "vault" {
     name = "rv-test-${data.azurerm_resource_group.ASR_rg.location}"
     location = data.azurerm_resource_group.ASR_rg.location
+    resource_group_name = data.azurerm_resource_group.ASR_rg.name
 }
