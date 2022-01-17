d7a26e68503a0a17380957f8fe911a785453f39c
Ed Haszard Morris <ed.haszard@ihc.org.nz>
2022-01-17 13:41:10 +1300

    removed location arg from main main call to vault module

--- main.tf
+++ main.tf
@@ -122,5 +122,4 @@ module "azvm1" {
 ###################################################
 module "vault1" {
   source = ".//modules/az_recovery_vault"
-  location = module.asr_resourcegr.location
 }
